9d7003a23446ec4d5e519e845c3f8d163c58eaeb
Andrey Bogachevskiy <ace7upp@gmail.com>
2024-03-18 19:29:48 +0300

    Скорректирован вызов страницы ошибки

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -2,22 +2,16 @@
 
 namespace app\controllers;
 
-use Yii;
-use yii\filters\AccessControl;
 use yii\web\Controller;
-use yii\web\Response;
-use yii\filters\VerbFilter;
+use yii\web\ErrorAction;
 
 class SiteController extends Controller
 {
-    /**
-     * {@inheritdoc}
-     */
     public function actions()
     {
         return [
             'error' => [
-                'class' => yii\web\ErrorAction::class,
+                'class' => ErrorAction::class,
             ],
         ];
     }
