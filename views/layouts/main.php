8733d12850e6292251941e2fc739556efd62ab87
Andrey Bogachevskiy <ace7upp@gmail.com>
2024-03-18 19:59:23 +0300

    В основной лайаут добавлена навигационная полоса и подвал

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -6,6 +6,8 @@
 use app\assets\AppAsset;
 use yii\bootstrap5\Breadcrumbs;
 use yii\bootstrap5\Html;
+use yii\bootstrap5\Nav;
+use yii\bootstrap5\NavBar;
 
 AppAsset::register($this);
 
@@ -30,15 +32,52 @@ $this->registerLinkTag(['rel' => 'icon', 'type' => 'image/x-icon', 'href' => Yii
 
 <body class="d-flex flex-column h-100">
     <?php $this->beginBody() ?>
-    <main id="main" class="flex-shrink-0" role="main">
+    <header>
+
+        <?php
+            NavBar::begin(
+                [
+                    'brandLabel' => \Yii::$app->name,
+                    'brandUrl'   => \Yii::$app->homeUrl,
+                    'options'    => [
+                        'class' => 'navbar-inverse navbar-fixed-top navbar-light bg-warning',
+                    ],
+                ]
+            );
+
+            echo Nav::widget([
+                'encodeLabels' => false,
+                'options'      => ['class' => 'navbar-nav navbar-right'],
+                'items'        => [
+                    [
+                        'label' => 'Вход',
+                        'url' => [
+                            '#',
+                        ],
+                    ],
+                ],
+            ]);
+
+            NavBar::end();
+        ?>
+    </header>
+
+    <main id="main" class="flex-shrink-0 mt-4" role="main">
         <div class="container">
-            <?php if (!empty($this->params['breadcrumbs'])): ?>
+            <?php if (empty($this->params['breadcrumbs']) === false): ?>
                 <?= Breadcrumbs::widget(['links' => $this->params['breadcrumbs']]) ?>
             <?php endif ?>
             <?= $content ?>
         </div>
     </main>
 
+    <footer class="navbar fixed-bottom navbar-light bg-light">
+        <div class="container">
+            <span class="navbar-text">
+              &copy; Практикум "ЭФКО Цифровые решения" <?= date('Y') ?>
+            </span>
+        </div>
+    </footer>
 
     <?php $this->endBody() ?>
 </body>
